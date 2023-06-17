bd2add2a4bd61e84c9237faa6fda8c93489b61cf
Mikhub1 <122497460+Mikhub1@users.noreply.github.com>
2023-06-17 10:54:01 -0230

    Add files via upload
    Music stuff

--- music_1.py
+++ music_1.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template, request, json, redirect, url_for
 import requests
 import urllib.request, json
 from api_address import  generate_page
+from get_data import *
 import os
 
 app = Flask(__name__)
+
+@app.route("/")
+def launch():
+    return render_template("music.html")
 
 @app.route("/music", methods=['POST', 'GET'])
 def home():
@@ -16,15 +21,18 @@ def home():
         response = requests.get(url)
         data = response.json()
         print(data)
-   
-        artist_info = {}
-
-        if data['info'] != []: 
-            artist_details = data['info'][0]
-            print(artist_details)
-
-            return render_template('music.html')
-        return render_template('music.html')
+        name = get_name()
+        print(name)
+        identity = get_id()
+        print(identity)
+        picture = get_picture(data)
+        print(picture)
+        picture_2 = get_picture_medium(data)
+        print(picture_2)
+        tracklist= tracklist(data)
+        print(tracklist)
+        return render_template('music.html', artist=artist, identity=identity, picture=picture, picture_2=picture_2, tracklist=tracklist)
+    return render_template('music.html')
 
 
 if __name__ == '__main__':
